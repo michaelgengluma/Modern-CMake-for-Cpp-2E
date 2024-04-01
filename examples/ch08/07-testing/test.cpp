fc28b41f44e8a90cd9fc8a16b1a74fdfd52e3b9a
Drakemor <Drakemor@users.noreply.github.com>
2024-04-01 12:24:33 +0000

    Ch07: Enhance example with better logging to explain what's going on

--- examples/ch08/07-testing/test.cpp
+++ examples/ch08/07-testing/test.cpp
@@ -1,13 +1,21 @@
 #include <iostream>
-extern int start_program(int, const char**);
+extern int start_program(int, const char **);
 using namespace std;
-int main() {
+int main()
+{
+  cout << "Test 1: Passing zero arguments to start_program:\n";
   auto exit_code = start_program(0, nullptr);
   if (exit_code == 0)
-    cout << "Non-zero exit code expected" << endl;
+    cout << "Test FAILED: Unexpected zero exit code.\n";
+  else
+    cout << "Test PASSED: Non-zero exit code returned.\n";
+  cout << endl;
 
-  const char* arguments[2] = {"hello", "world"};
+  cout << "Test 2: Passing 2 arguments to start_program:\n";
+  const char *arguments[2] = {"hello", "world"};
   exit_code = start_program(2, arguments);
   if (exit_code != 0)
-    cout << "Zero exit code expected" << endl;
+    cout << "Test FAILED: Unexpected non-zero exit code\n";
+  else
+    cout << "Test PASSED\n";
 }
