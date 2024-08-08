b66db9ff5f9bb3a13f99fab3dd955c0ab3a76ed4
Michael Geng <michael.geng@lumachain.io>
2024-08-08 11:07:15 +1000

    added 02-odr-success

---
+++ examples/ch08/02-odr-success/one.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+#include "shared.h"
+
+int i = 1;
+int main() {
+  std::cout << i << std::endl;
+}
---
+++ examples/ch08/02-odr-success/shared.h
@@ -0,0 +1,6 @@
+#ifndef SHARED_H
+#define SHARED_H
+
+extern int i;  // Declaration of the variable 'i'
+
+#endif // SHARED_H
